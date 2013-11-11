25aa3dc5aa55154dc385057c6dd79476c3fbe877
Brian Pollack <brian.pollack@cern.ch>
2013-11-11 08:06:58 -0600

    first version of tmva py

--- mva/TrainMVA_HZG.py
+++ mva/TrainMVA_HZG.py
@@ -188,11 +188,10 @@ def TrainMva(myMethodList = '', _signalName, _bgName, _numSignalTrain = 0, _numB
     signal_test = sigFile_test.Get('varMVA')
     background_test = bgFile_test.Get('varMVA')
 
-    factory.AddSignalTree(signal_train, signalWeight * 2, 'Training')
-    factory.AddBackgroundTree(background_train, backgroundWeight * 2, 'Training')
-    factory.AddSignalTree(signal_test, signalWeight * 2, 'Test')
-    factory.AddBackgroundTree(background_test, backgroundWeight * 2, 'Test')
-  }
+    factory.AddSignalTree(signal_train, signalWeight, 'Training')
+    factory.AddBackgroundTree(background_train, backgroundWeight, 'Training')
+    factory.AddSignalTree(signal_test, signalWeight, 'Test')
+    factory.AddBackgroundTree(background_test, backgroundWeight, 'Test')
 
   # Weights stored in the input ntuples to be used for event weighting.
   # For mixed bg samples these will contain scaling factors that ensure the
@@ -202,26 +201,243 @@ def TrainMva(myMethodList = '', _signalName, _bgName, _numSignalTrain = 0, _numB
   factory.SetSignalWeightExpression('scaleFactor')
   factory.SetBackgroundWeightExpression('scaleFactor')
 
-  if (!USE_SEPARATE_TRAIN_TEST_FILES) {
+  if (!USE_SEPARATE_TRAIN_TEST_FILES):
     factory.PrepareTrainingAndTestTree(addCutsSig, addCutsBg,
         'nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V') # I used this one when reading train/test from same file
-  } else {
+  else:
     # I prefer using separate files for training/testing, so this is just for completeness.
-
     # note that here the signal and the bg cuts are the same, so we can use this form
+    _numSignalTrain = signal_train.GetEntries()
+    _numBgTrain = background_train.GetEntries()
+    _numSignalTest = signal_test.GetEntries()
+    _numBgTest = background_test.GetEntries()
     factory.PrepareTrainingAndTestTree(addCutsSig,
         _numSignalTrain, _numBgTrain, _numSignalTest, _numBgTest,
         'SplitMode=Random:NormMode=NumEvents:!V')
-  }
 
 
-  std::cout << '==> Booking the methods ' << std::endl
+  print '==> Booking the methods'
   # ---- Book MVA methods
   #
   # Please lookup the various method configuration options in the corresponding cxx files, eg:
   # src/MethoCuts.cxx, etc, or here: http:#tmva.sourceforge.net/optionRef.html
   # it is possible to preset ranges in the option string in which the cut optimisation should be done:
   # '...:CutRangeMin[2]=-1:CutRangeMax[2]=1'...', where [2] is the third input variable
+
+  # Cut optimisation
+  if (Use['Cuts']):
+    factory.BookMethod(ROOT.TMVA.Types.kCuts, 'Cuts',
+        '!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart')
+
+  if (Use['CutsD']):
+    factory.BookMethod(ROOT.TMVA.Types.kCuts, 'CutsD',
+        '!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=Decorrelate')
+
+  if (Use['CutsPCA']):
+    factory.BookMethod(ROOT.TMVA.Types.kCuts, 'CutsPCA',
+        '!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=PCA')
+
+  if (Use['CutsGA']):
+    factory.BookMethod(ROOT.TMVA.Types.kCuts, 'CutsGA',
+        'H:!V:FitMethod=GA:CutRangeMin[0]=-10:CutRangeMax[0]=10:VarProp[1]=FMax:EffSel:Steps=30:Cycles=3:PopSize=400:SC_steps=10:SC_rate=5:SC_factor=0.95')
+
+  if (Use['CutsSA']):
+    factory.BookMethod(ROOT.TMVA.Types.kCuts, 'CutsSA',
+        '!H:!V:FitMethod=SA:EffSel:MaxCalls=150000:KernelTemp=IncAdaptive:InitialTemp=1e+6:MinTemp=1e-6:Eps=1e-10:UseDefaultScale')
+
+  # Likelihood ('naive Bayes estimator')
+  if (Use['Likelihood']):
+    factory.BookMethod(ROOT.TMVA.Types.kLikelihood, 'Likelihood',
+        'H:!V:TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmoothBkg[1]=10:NSmooth=1:NAvEvtPerBin=50')
+
+  # Decorrelated likelihood
+  if (Use['LikelihoodD']):
+    factory.BookMethod(ROOT.TMVA.Types.kLikelihood, 'LikelihoodD',
+        '!H:!V:TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=50:VarTransform=Decorrelate')
+
+  # PCA-transformed likelihood
+  if (Use['LikelihoodPCA']):
+    factory.BookMethod(ROOT.TMVA.Types.kLikelihood, 'LikelihoodPCA',
+        '!H:!V:!TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=50:VarTransform=PCA')
+
+  # Use a kernel density estimator to approximate the PDFs
+  if (Use['LikelihoodKDE']):
+    factory.BookMethod(ROOT.TMVA.Types.kLikelihood, 'LikelihoodKDE',
+        '!H:!V:!TransformOutput:PDFInterpol=KDE:KDEtype=Gauss:KDEiter=Adaptive:KDEFineFactor=0.3:KDEborder=None:NAvEvtPerBin=50')
+
+  # Use a variable-dependent mix of splines and kernel density estimator
+  if (Use['LikelihoodMIX']):
+    factory.BookMethod(ROOT.TMVA.Types.kLikelihood, 'LikelihoodMIX',
+        '!H:!V:!TransformOutput:PDFInterpolSig[0]=KDE:PDFInterpolBkg[0]=KDE:PDFInterpolSig[1]=KDE:PDFInterpolBkg[1]=KDE:PDFInterpolSig[2]=Spline2:PDFInterpolBkg[2]=Spline2:PDFInterpolSig[3]=Spline2:PDFInterpolBkg[3]=Spline2:KDEtype=Gauss:KDEiter=Nonadaptive:KDEborder=None:NAvEvtPerBin=50')
+
+  # Test the multi-dimensional probability density estimator
+  # here are the options strings for the MinMax and RMS methods, respectively:
+  #      '!H:!V:VolumeRangeMode=MinMax:DeltaFrac=0.2:KernelEstimator=Gauss:GaussSigma=0.3' )
+  #      '!H:!V:VolumeRangeMode=RMS:DeltaFrac=3:KernelEstimator=Gauss:GaussSigma=0.3' )
+  if (Use['PDERS']):
+    factory.BookMethod(ROOT.TMVA.Types.kPDERS, 'PDERS',
+        '!H:!V:NormTree=T:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=400:NEventsMax=600')
+
+  if (Use['PDERSD']):
+    factory.BookMethod(ROOT.TMVA.Types.kPDERS, 'PDERSD',
+        '!H:!V:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=400:NEventsMax=600:VarTransform=Decorrelate')
+
+  if (Use['PDERSPCA']):
+    factory.BookMethod(ROOT.TMVA.Types.kPDERS, 'PDERSPCA',
+        '!H:!V:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=400:NEventsMax=600:VarTransform=PCA')
+
+  # Multi-dimensional likelihood estimator using self-adapting phase-space binning
+  if (Use['PDEFoam']):
+    factory.BookMethod(ROOT.TMVA.Types.kPDEFoam, 'PDEFoam',
+        '!H:!V:SigBgSeparate=F:TailCut=0.001:VolFrac=0.0666:nActiveCells=500:nSampl=2000:nBin=5:Nmin=100:Kernel=None:Compress=T')
+
+  if (Use['PDEFoamBoost']):
+    factory.BookMethod(ROOT.TMVA.Types.kPDEFoam, 'PDEFoamBoost',
+        '!H:!V:Boost_Num=30:Boost_Transform=linear:SigBgSeparate=F:MaxDepth=4:UseYesNoCell=T:DTLogic=MisClassificationError:FillFoamWithOrigWeights=F:TailCut=0:nActiveCells=500:nBin=20:Nmin=400:Kernel=None:Compress=T')
+
+  # K-Nearest Neighbour classifier (KNN)
+  if (Use['KNN']):
+    factory.BookMethod(ROOT.TMVA.Types.kKNN, 'KNN',
+        'H:nkNN=20:ScaleFrac=0.8:SigmaFact=1.0:Kernel=Gaus:UseKernel=F:UseWeight=T:!Trim')
+
+  # H-Matrix (chi2-squared) method
+  if (Use['HMatrix']):
+    factory.BookMethod(ROOT.TMVA.Types.kHMatrix, 'HMatrix', '!H:!V:VarTransform=None')
+
+  # Linear discriminant (same as Fisher discriminant)
+  if (Use['LD']):
+    factory.BookMethod(ROOT.TMVA.Types.kLD, 'LD', 'H:!V:VarTransform=None:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10')
+
+  # Fisher discriminant (same as LD)
+  if (Use['Fisher']):
+    factory.BookMethod(ROOT.TMVA.Types.kFisher, 'Fisher', 'H:!V:Fisher:VarTransform=None:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10')
+
+  # Fisher with Gauss-transformed input variables
+  if (Use['FisherG']):
+    factory.BookMethod(ROOT.TMVA.Types.kFisher, 'FisherG', 'H:!V:VarTransform=Gauss')
+
+  # Composite classifier: ensemble (tree) of boosted Fisher classifiers
+  if (Use['BoostedFisher']):
+    factory.BookMethod(ROOT.TMVA.Types.kFisher, 'BoostedFisher',
+        'H:!V:Boost_Num=20:Boost_Transform=log:Boost_Type=AdaBoost:Boost_AdaBoostBeta=0.2:!Boost_DetailedMonitoring')
+
+  # Function discrimination analysis (FDA) -- test of various fitters - the recommended one is Minuit (or GA or SA)
+  if (Use['FDA_MC']):
+    factory.BookMethod(ROOT.TMVA.Types.kFDA, 'FDA_MC',
+        'H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1)(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=MC:SampleSize=100000:Sigma=0.1');
+
+  if (Use['FDA_GA']): # can also use Simulated Annealing (SA) algorithm (see Cuts_SA options])
+    factory.BookMethod(ROOT.TMVA.Types.kFDA, 'FDA_GA',
+        'H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1)(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=GA:PopSize=300:Cycles=3:Steps=20:Trim=True:SaveBestGen=1');
+
+  if (Use['FDA_SA']): # can also use Simulated Annealing (SA) algorithm (see Cuts_SA options])
+    factory.BookMethod(ROOT.TMVA.Types.kFDA, 'FDA_SA',
+        'H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1)(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=SA:MaxCalls=15000:KernelTemp=IncAdaptive:InitialTemp=1e+6:MinTemp=1e-6:Eps=1e-10:UseDefaultScale');
+
+  if (Use['FDA_MT']):
+    factory.BookMethod(ROOT.TMVA.Types.kFDA, 'FDA_MT',
+        'H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1)(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=2:UseImprove:UseMinos:SetBatch');
+
+  if (Use['FDA_GAMT']):
+    factory.BookMethod(ROOT.TMVA.Types.kFDA, 'FDA_GAMT',
+        'H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1)(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=GA:Converger=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=0:!UseImprove:!UseMinos:SetBatch:Cycles=1:PopSize=5:Steps=5:Trim');
+
+  if (Use['FDA_MCMT']):
+    factory.BookMethod(ROOT.TMVA.Types.kFDA, 'FDA_MCMT',
+        'H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1)(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=MC:Converger=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=0:!UseImprove:!UseMinos:SetBatch:SampleSize=20');
+
+  # TMVA ANN: MLP (recommended ANN) -- all ANNs in TMVA are Multilayer Perceptrons
+  if (Use['MLP']):
+    factory.BookMethod(ROOT.TMVA.Types.kMLP, 'MLP', '!H:!V:NeuronType=tanh:VarTransform=N:NCycles=500:HiddenLayers=N:TestRate=10')
+
+  if (Use['MLPBFGS']):
+    factory.BookMethod(ROOT.TMVA.Types.kMLP, 'MLPBFGS', 'H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:TrainingMethod=BFGS:!UseRegulator')
+
+  if (Use['MLPBNN']):
+    factory.BookMethod(ROOT.TMVA.Types.kMLP, 'MLPBNN', 'H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:TrainingMethod=BFGS:UseRegulator') # BFGS training with bayesian regulators
+  # reduced epochs from 600 (AA)
+
+
+  # CF(Clermont-Ferrand)ANN
+  if (Use['CFMlpANN']):
+    factory.BookMethod(ROOT.TMVA.Types.kCFMlpANN, 'CFMlpANN', '!H:!V:NCycles=2000:HiddenLayers=N+1,N') # n_cycles:#nodes:#nodes:...
+
+  # Tmlp(Root)ANN
+  if (Use['TMlpANN']):
+    factory.BookMethod(ROOT.TMVA.Types.kTMlpANN, 'TMlpANN', '!H:!V:NCycles=200:HiddenLayers=N+1,N:LearningMethod=BFGS:ValidationFraction=0.3') # n_cycles:#nodes:#nodes:...
+
+  # Support Vector Machine
+  if (Use['SVM']):
+    factory.BookMethod(ROOT.TMVA.Types.kSVM, 'SVM', 'Gamma=0.25:Tol=0.001:VarTransform=Norm')
+
+  # Boosted Decision Trees - DEFAULT
+  #    if (Use['BDTG']): # Gradient Boost . Default parameters
+  #        factory.BookMethod(ROOT.TMVA.Types.kBDT, 'BDTG',
+  #            '!H:!V:NTrees=1000:BoostType=Grad:Shrinkage=0.10:UseBaggedGrad:GradBaggingFraction=0.5:nCuts=20:NNodesMax=5')
+
+
+  # here is a version with modified parameters
+  if (Use['BDTG']): # Gradient Boost
+    factory.BookMethod(ROOT.TMVA.Types.kBDT, 'BDTG',
+        '!H:!V:NTrees=1000:BoostType=Grad:Shrinkage=0.10:UseBaggedGrad:GradBaggingFraction=0.5:nCuts=20:NNodesMax=8:nEventsMin=12')
+
+
+  if (Use['BDT']): # Adaptive Boost
+    factory.BookMethod(ROOT.TMVA.Types.kBDT, 'BDT',
+        '!H:!V:NTrees=400:nEventsMin=40:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=1:SeparationType=GiniIndex:nCuts=20')
+
+
+  if (Use['BDTB']): # Bagging
+    factory.BookMethod(ROOT.TMVA.Types.kBDT, 'BDTB',
+        '!H:!V:NTrees=400:BoostType=Bagging:SeparationType=GiniIndex:nCuts=20:PruneMethod=NoPruning')
+
+  if (Use['BDTD']): # Decorrelation + Adaptive Boost
+    factory.BookMethod(ROOT.TMVA.Types.kBDT, 'BDTD',
+        '!H:!V:NTrees=400:nEventsMin=400:MaxDepth=3:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=20:PruneMethod=NoPruning:VarTransform=Decorrelate')
+
+  if (Use['BDTF']): # Allow Using Fisher discriminant in node splitting for (strong) linearly correlated variables
+    factory.BookMethod(ROOT.TMVA.Types.kBDT, 'BDTMitFisher',
+        '!H:!V:NTrees=50:nEventsMin=150:UseFisherCuts:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:SeparationType=GiniIndex:nCuts=20:PruneMethod=NoPruning')
+
+  # RuleFit -- TMVA implementation of Friedman's method
+  if (Use['RuleFit']):
+    factory.BookMethod(ROOT.TMVA.Types.kRuleFit, 'RuleFit',
+        'H:!V:RuleFitModule=RFTMVA:Model=ModRuleLinear:MinImp=0.001:RuleMinDist=0.001:NTrees=20:fEventsMin=0.01:fEventsMax=0.5:GDTau=-1.0:GDTauPrec=0.01:GDStep=0.01:GDNSteps=10000:GDErrScale=1.02')
+
+  # For an example of the category classifier usage, see: TMVAClassificationCategory
+
+  # --------------------------------------------------------------------------------------------------
+
+  # ---- Now you can optimize the setting (configuration) of the MVAs using the set of training events
+
+  # factory.OptimizeAllMethods('SigEffAt001','Scan')
+  # factory.OptimizeAllMethods('ROCIntegral','GA')
+
+  # --------------------------------------------------------------------------------------------------
+
+  # ---- Now you can tell the factory to train, test, and evaluate the MVAs
+
+  # Train MVAs using the set of training events
+  factory.TrainAllMethods()
+
+  # ---- Evaluate all MVAs using the set of test events
+  factory.TestAllMethods()
+
+  # ----- Evaluate and compare performance of all configured MVAs
+  factory.EvaluateAllMethods()
+
+  # --------------------------------------------------------------
+
+  # Save the output
+  outputFile.Close()
+
+  std.cout << '==> Wrote root file: ' << outputFile.GetName() << std.endl
+  std.cout << '==> TrainMva is done!' << std.endl
+
+  # Launch the GUI for the root macro
+  # make it lightweight for batch jobs and skip loading this script . for interactive include
+  # TMVAGui.C which is currently commented out.
+  # if (!gROOT.IsBatch()) TMVAGui(outFileName)
 
 
 
