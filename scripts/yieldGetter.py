d8264b901267ead011ee6080463159aa2e359151
Brian Pollack <brian.pollack@cern.ch>
2014-02-03 15:55:16 -0600

    full yield script working

--- scripts/yieldGetter.py
+++ scripts/yieldGetter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from ROOT import *
 import numpy as np
+import sys
+from PlottingClasses import AutoVivification
 
 def LumiXSScale(name,lepton,year,thisFile):
   '''Outputs scale for MC with respect to lumi and XS'''
@@ -8,8 +10,8 @@ def LumiXSScale(name,lepton,year,thisFile):
   if name is 'DATA': return 1
 
   lumi = 0
-  if lepton is 'mu': lumi = 19.672
-  elif lepton is 'el': lumi = 19.711
+  if lepton is 'MuMu': lumi = 19.672
+  elif lepton is 'EE': lumi = 19.711
   else: raise NameError('LumiXSScale lepton incorrect')
 
   scaleDict = AutoVivification()
@@ -30,26 +32,43 @@ def LumiXSScale(name,lepton,year,thisFile):
   return scale
 
 
-def DoYield(fileName):
+def DoYield(fileName, secondName = None):
   print fileName
   signal = 'M125'
   '''Get full yield breakdown for all categories, data, signal'''
   leptons = ['MuMu','EE']
   dataTypes = ['Data','Signal']
   for lep in leptons:
-    print lep
+    print '  ',lep
     thisFile = TFile('../HiggsZGAnalyzer/batchHistos/higgsHistograms_'+lep+'2012ABCD_'+fileName+'.root')
+    if secondName:
+      secondFile = TFile('../HiggsZGAnalyzer/batchHistos/higgsHistograms_'+lep+'2012ABCD_'+secondName+'.root')
     for dataType in dataTypes:
-      if dataType == 'Data': suffix = '_DATA'
-      elif dataType == 'Signal': suffix = '_Signal2012gg'+signal
-      rawTotal = thisFile.Get('h1_InvariantMass'+suffix).Integral()
-      scaledTotal =
-      print rawTotal
+      if dataType == 'Data': suffix = 'DATA'
+      elif dataType == 'Signal': suffix = 'Signal2012gg'+signal
+      print '    ',suffix
+      rawTotal = thisFile.Get('h1_InvariantMass_'+suffix).Integral()
+      scaledTotal = rawTotal*LumiXSScale(suffix, lep, '2012', thisFile)
+      print '      ','rawTotal: {0:.0f} scaledTotal: {1:.2f}'.format(rawTotal,scaledTotal)
       for i in range(1,10):
-        if thisFile.Get('h1_threeBodyMassCAT'+str(i)+suffix):
-          print thisFile.Get('h1_threeBodyMassCAT'+str(i)+suffix)
+        if thisFile.Get('h1_threeBodyMassCAT'+str(i)+'_'+suffix):
+          rawTotalCat = thisFile.Get('h1_threeBodyMassCAT'+str(i)+'_'+suffix).Integral()
+          scaledTotalCat = rawTotalCat*LumiXSScale(suffix, lep, '2012', thisFile)
+          percentage = (rawTotalCat/rawTotal)*100
+          if secondName:
+            rawTotalCatSecond = secondFile.Get('h1_threeBodyMassCAT'+str(i)+'_'+suffix).Integral()
+            percentageChange = (rawTotalCat/rawTotalCatSecond)*100
+
+          if secondName:
+            print '      ','CAT{0} rawTotal: {1:4.0f} scaledTotal: {2:7.2f} percent: {3:4.1f}% percentCh: {4:.1f}%'.format(i,rawTotalCat,scaledTotalCat,percentage,percentageChange)
+          else:
+            print '      ','CAT{0} rawTotal: {1:4.0f} scaledTotal: {2:7.2f} percent: {3:.1f}%'.format(i,rawTotalCat,scaledTotalCat,percentage)
 
 
 if __name__ == '__main__':
- DoYield('02-03-14')
+  name = str(sys.argv[1])
+  secondName = None
+  if sys.argv[2]: secondName = sys.argv[2]
+  DoYield(name, secondName)
+
 
